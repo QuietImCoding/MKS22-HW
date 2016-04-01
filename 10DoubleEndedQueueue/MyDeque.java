14a115230d1e4cc98b4e70eccd377c935a918e32
Daniel Monteagudo <dmonte3210@gmail.com>
2016-04-01 12:57:37 -0400

    Made two add methods, added main and toString

--- 10DoubleEndedQueueue/MyDeque.java
+++ 10DoubleEndedQueueue/MyDeque.java
@@ -1,24 +1,35 @@
 public class MyDeque<T> {
+
     private Object[] data;
     private int start, end, size;
   
     private MyDeque() {
 	data = new Object[8];
-	start = 0;
-	end = 0;
+	start = data.length/2;
+	end = data.length/2;
 	size = 0;
     }
 
     public void addFirst(T value) {
-	
+	if(start>0) {
+	    start--;
+	} else {
+	    start = data.length-1;
+	}
+	data[start] = value;
     }
 
     public void addLast(T value) {
-
+	if(end<(data.length-1)){
+	    end++;
+	} else {
+	    end = 0;
+	}
+	data[end] = value;
     }
 
     public T removeFirst() {
-	
+	T first = data[start]
     }
 
     public T removeLast() {
@@ -31,5 +42,26 @@ public class MyDeque<T> {
 
     public T getLast() {
 
+    }
+
+    public String toString() {
+	String s = "[ ";
+	for(int i = 0;i < data.length;i++) {
+	    if(data[i]==null){ 
+		s+= "X";
+	    } else {
+		s+= data[i];
+	    }
+	}
+	s+=" ]";
+	return s;
+    }
+
+    public static void main(String[]args) {
+	MyDeque dq = new MyDeque();
+	for (int i = 0; i < 8; i++) {
+	    dq.add(10);
+	    System.out.println(dq);
+	}
     }
 }
