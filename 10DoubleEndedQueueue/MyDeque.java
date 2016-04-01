72f5971b44c514a66d89c6d3095f8f0bc0736432
Daniel Monteagudo <dmonte3210@gmail.com>
2016-04-01 12:46:13 -0400

    Made method skeletons (of the spooky scary variety)

---
+++ 10DoubleEndedQueueue/MyDeque.java
@@ -0,0 +1,35 @@
+public class MyDeque<T> {
+    private Object[] data;
+    private int start, end, size;
+  
+    private MyDeque() {
+	data = new Object[8];
+	start = 0;
+	end = 0;
+	size = 0;
+    }
+
+    public void addFirst(T value) {
+	
+    }
+
+    public void addLast(T value) {
+
+    }
+
+    public T removeFirst() {
+	
+    }
+
+    public T removeLast() {
+
+    }
+
+    public T getFirst() {
+	
+    }
+
+    public T getLast() {
+
+    }
+}
