7d661b8255e6059d2b249bd5f9aec2789fc31a37
Daniel Monteagudo <dmonte3210@gmail.com>
2016-04-03 15:03:05 -0400

    have removes working (sorta?) :/

--- 10DoubleEndedQueueue/MyDeque.java
+++ 10DoubleEndedQueueue/MyDeque.java
@@ -1,20 +1,34 @@
+import java.util.*;
+
 public class MyDeque<T> {
 
     private Object[] data;
     private int start, end, size;
-  
+
+    @SuppressWarnings("unchecked")
     private MyDeque() {
-	data = new Object[8];
+	data = (T[]) new Object[8];
 	start = data.length/2;
 	end = data.length/2;
 	size = 0;
     }
 
+    public void grow() {
+	Object[] blub = (T[]) new Object[data.length*2];
+	for (int i = 0; i < data.length; i++) {
+	    blub[i] = data[i];
+	}
+	data = blub;
+    }
+    
     public void addFirst(T value) {
 	if(start>0) {
 	    start--;
 	} else {
 	    start = data.length-1;
+	}
+	if(data[start]!=null) {
+	    grow();
 	}
 	data[start] = value;
     }
@@ -29,19 +43,25 @@ public class MyDeque<T> {
     }
 
     public T removeFirst() {
+	if(start>0){
+	    start--;
+	}else{
+	    start=data.length-1;
+	}
 	T first = (T)data[start];
-	data[start] = null;
-	if(start<data.length){
-	    start++;
-	}
-	else {
-	    start = 0;
-	}
+	data[start]=null;
 	return first;
     }
 
     public T removeLast() {
-	return null;
+	T last = (T)data[end];
+	data[end]=null;
+	if(end<data.length-1){
+	    end++;
+	}else{
+	    end = 0;
+	}
+	return last;
     }
 
     public T getFirst() {
@@ -69,7 +89,13 @@ public class MyDeque<T> {
     public static void main(String[]args) {
 	MyDeque<Integer> dq = new MyDeque<Integer>();
 	for (int i = 0; i < 8; i++) {
-	    dq.addFirst(10);
+	    dq.addFirst(i);
+	    System.out.println(dq);
+	}
+	dq.grow();
+	System.out.println(dq);
+	for (int i = 0; i < 8; i++) {
+	    System.out.println(dq.removeLast());
 	    System.out.println(dq);
 	}
     }
